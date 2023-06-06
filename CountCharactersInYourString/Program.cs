1426365df17d69efa50ee62cfa5ffdfdd62bb0dc
KateOblomova <125303630+KateOblomova@users.noreply.github.com>
2023-06-06 22:17:19 +0300

    Add files via upload

---
+++ CountCharactersInYourString/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+DESCRIPTION:
+
+The main idea is to count all the occurring characters in a string. 
+If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+ 
+ */
+
+static Dictionary<char, int> Count(string str)
+{
+    Dictionary<char, int> dictionary = new Dictionary<char, int>();
+    char[] chars = str.ToCharArray();
+
+    foreach (char c in chars)
+    {
+        if (dictionary.ContainsKey(c))
+        {
+            dictionary[c]++;
+        }
+        else { dictionary.Add(c, 1); }
+    }
+    return dictionary;
+}
